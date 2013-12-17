0356101181959f9ee28a88b31107dde1addc9fd7
Matthew Lien <matt.gru@gmail.com>
2013-12-17 17:23:19 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,97 @@
+/**
+ * Module dependencies.
+ */
+
+var express 		= require('express')
+  , stylus 			= require('stylus')
+  , nib 			= require('nib')
+  , http 			= require('http')
+  , path 			= require('path')
+  , async 			= require('async')
+  , expressUglify 	= require('express-uglify');
+
+var api 		= require('./routes/api')
+  , routes 		= require('./routes')
+  , stats 		= require('./routes/stats')
+  , config 		= require('./libs/Config');
+
+var publicPath 	= path.join(__dirname, 'public')
+  , app 		= express();
+var server 		= http.createServer(app);
+
+config().then(function(conf) {
+	app.config = conf;
+
+	// all environments
+	app.set('host', app.config.host);
+	app.set('port', app.config.port);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.urlencoded())
+	app.use(express.json())
+	app.use(express.methodOverride());
+
+	app.use(app.config.webRoot, stylus.middleware({src: publicPath, compile: function(str, path) {
+		return stylus(str).set('filename', path) .use(nib())
+	}}));
+
+	app.configure('development', function() {
+		app.locals.pretty = true;
+		app.use(express.responseTime());
+		app.use(express.logger('dev'));
+		app.use(app.config.webRoot, express.static(publicPath));
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
+
+	app.configure('production', function() {
+		app.enable('trust proxy');
+
+		if(app.config.logHttpRequests) {
+			app.use(express.logger());
+		}
+
+		app.use(app.config.webRoot, expressUglify.middleware({ src: publicPath, logger: function() {
+			this.log = function() {
+
+			};
+		} }));
+
+		var oneYear = 31557600000;
+		app.use(app.config.webRoot, express.static(publicPath, { maxAge: oneYear }));
+		app.use(express.errorHandler());
+	});
+
+	app.use(app.router);
+}).then(function() {
+
+	var webRoot = app.config.webRoot;
+
+	app.get(webRoot + '/', routes.index);
+	app.get(webRoot + '/install', routes.install);
+
+	app.get(webRoot + '/api/plex/currentlyWatching', api.plex.currentlyWatching);
+	app.get(webRoot + '/api/plex/poster', api.plex.poster);
+	app.get(webRoot + '/api/plex/recentlyAddedMovies', api.plex.recentlyAddedMovies);
+	app.get(webRoot + '/api/plex/recentlyAired', api.plex.recentlyAired);
+
+	app.get(webRoot + '/api/sickbeard/poster', api.sickbeard.poster);
+	app.get(webRoot + '/api/sickbeard/showsStats', api.sickbeard.showsStats);
+	app.get(webRoot + '/api/sickbeard/upcoming', api.sickbeard.upcoming);
+
+	app.get(webRoot + '/stats/bandwidth', stats.bandwidth);
+	app.get(webRoot + '/stats/cpu', stats.cpu);
+	app.get(webRoot + '/stats/disks', stats.disks);
+	app.get(webRoot + '/stats/memory', stats.memory);
+	app.get(webRoot + '/stats/services', stats.services);
+	app.get(webRoot + '/stats/weather', stats.weather);
+
+}).then(function() {
+	server.listen(app.get('port'), app.get('host'), function() {
+		var uri = app.get('host') + ':' + app.get('port') + app.config.webRoot;
+
+		console.log('StatusBoard'.yellow, 'is running at',  uri.cyan);
+	});
+}).otherwise(function(reason) {
+	console.log('Error has occurred when trying to validate configuration data:'.red, reason.message);
+});
